3f0c596cb01c4cac86630d63e3d490909a9de158
nhathanguyenhoang <nguyenhoangnhatha911@gmail.com>
2026-04-27 23:22:13 +0700

    build# show report on pipeline no need to download

--- playwright.config.js
+++ playwright.config.js
@@ -2,20 +2,29 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
-
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { open: 'never' }]],
+
+  reporter: process.env.CI
+    ? [
+        ['github'],
+        ['list', { printSteps: true }],
+        ['html', { open: 'never' }],
+      ]
+    : [
+        ['list', { printSteps: true }],
+        ['html', { open: 'never' }],
+      ],
 
   use: {
     baseURL: 'https://opensource-demo.orangehrmlive.com',
     headless: true,
-    // screenshot: 'only-on-failure',
-    // video: 'retain-on-failure',
-    screenshot: 'on',
-    video: 'on',
+    //screenshot: 'only-on-failure',
+    //video: 'retain-on-failure',
+    //screenshot: 'on',
+    //video: 'on',
     trace: 'on-first-retry',
   },
 });
